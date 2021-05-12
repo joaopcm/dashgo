1d91d198f10973e7ce429fe29a6ae3ab96a38f0f
Joao Melo <jopcmelo@gmail.com>
2021-05-12 15:22:03 -0300

    feat: add devtools from react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,21 +24,27 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
 
-    const users = data.users.map((user) => ({
-      ...user,
-      createdAt: new Date(user.createdAt).toLocaleDateString("en-US", {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
-      }),
-    }));
+      const users = data.users.map((user) => ({
+        ...user,
+        createdAt: new Date(user.createdAt).toLocaleDateString("en-US", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }),
+      }));
 
-    return users;
-  });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
